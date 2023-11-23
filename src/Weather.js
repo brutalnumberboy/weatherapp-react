b62b73e0fe5e731856821b45b239c6f1d32ccd68
brutalnumberboy <scandalzx@protonmail.com>
2023-11-23 21:21:52 +0100

    added more weather and general information

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,113 @@
-import './Weather.css';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import React from 'react';
-import LocationCityIcon from '@mui/icons-material/LocationCity';
-import PublicIcon from '@mui/icons-material/Public';
+import "./Weather.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import React from "react";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import PublicIcon from "@mui/icons-material/Public";
+import DeviceThermostatIcon from "@mui/icons-material/DeviceThermostat";
+import AirIcon from "@mui/icons-material/Air";
+import CloudIcon from "@mui/icons-material/Cloud";
+import WaterDropIcon from "@mui/icons-material/WaterDrop";
+import { Input } from "@mui/material";
 
-export default function Weather({ weatherData }){
-
-   
+export default function Weather({ weatherData }) {
   const card = (
     <React.Fragment>
       <CardContent color="aliceblue">
-        <Typography sx={{ fontSize: 24, fontFamily: 'Ubuntu'}} color="navy" gutterBottom>
-          Weather App
+        <Typography
+          sx={{ fontSize: 24, fontFamily: "Ubuntu" }}
+          color="navy"
+          gutterBottom
+        >
+          Weather
         </Typography>
-        <Typography variant="p" component="div">
-        </Typography>
-        <Typography sx={{ mb: 1.5, fontFamily: 'Ubuntu', display: 'inline-block' }} color="text.secondary" align="left">
-          <PublicIcon fontSize='small'/>Country: {weatherData.sys?.country}
+        <Typography variant="p" component="div"></Typography>
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.secondary"
+          align="left"
+        >
+          <PublicIcon fontSize="small" /> Country: {weatherData.sys?.country}
+        </Typography> 
+        <br />
+        
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.secondary"
+          align="left"
+        >
+          <PublicIcon fontSize="small" /> Timezone: {weatherData.timezone/3600} 
+        </Typography>  {/* need to figure out how to convert this to proper UTC*/}
+        <br />
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.secondary"
+          align="left"
+        >
+          <LocationCityIcon fontSize="small" /> City: {weatherData.name}
         </Typography>
         <br />
-        <Typography sx={{ mb: 1.5, fontFamily: 'Ubuntu', display: 'inline-block' }} color="text.secondary" align="left">
-          <LocationCityIcon fontSize='small'/>City: {weatherData.name} 
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.primary"
+          align="left"
+        >
+          Current Weather:
         </Typography>
         <br />
-        <Typography variant="body2">
-            {weatherData.main?.temp}
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.secondary"
+          align="left"
+        >
+          <DeviceThermostatIcon fontSize="small" /> Temperature:{" "}
+          {weatherData.main?.temp}
         </Typography>
+        <br />
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.secondary"
+          align="left"
+        >
+          <AirIcon fontSize="small" /> Wind Speed: {weatherData.wind?.speed}
+        </Typography>
+        <br />
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.secondary"
+          align="left"
+        >
+          <CloudIcon fontSize="small" /> Clouds: {weatherData.clouds?.all}
+        </Typography>
+        <br />
+        <Typography
+          sx={{ mb: 1.5, fontFamily: "Ubuntu", display: "inline-block" }}
+          color="text.secondary"
+          align="left"
+        >
+          <WaterDropIcon fontSize="small" /> Rain: {weatherData.rain?.["1h"]}
+        </Typography>
+        <br />
       </CardContent>
     </React.Fragment>
   );
 
-    return (
-        <>
-        <Card variant="outlined" className="card-weather" style={{backgroundColor: 'aliceblue'}}>{ card }</Card>
+  const input = (
+    <React.Fragment>
+      <input name="citySearch" />
+    </React.Fragment>
+  );
+  return (
+    <>
+      <Card
+        variant="outlined"
+        className="card-weather"
+        style={{ backgroundColor: "aliceblue" }}
+      >
+        {card}
+      </Card>
+      {/* <Input className="input">{ input }</Input> */}
     </>
-    );
+  );
 }
