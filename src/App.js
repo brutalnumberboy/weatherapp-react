08f84586b71879a8b55b1c3f1503598cc9b11c62
brutalnumberboy <scandalzx@protonmail.com>
2023-11-23 21:21:33 +0100

    formatting changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import { useEffect, useState } from 'react';
-import Weather from './Weather';
-import {API_KEY} from './OpenWeatherApi';
+import { useEffect, useState } from "react";
+import Weather from "./Weather";
+import { API_KEY } from "./OpenWeatherApi";
 function App() {
+  const [latitude, setLat] = useState([]);
+  const [longitude, setLong] = useState([]);
 
-    const [latitude, setLat] = useState([]);
-    const [longitude, setLong] = useState([]);
+  const [weather, setWeather] = useState([]);
 
-    const [weather, setWeather] = useState([]);
+  useEffect(() => {
+    function success(position) {
+      setLat(position.coords.latitude.toFixed(2));
+      setLong(position.coords.longitude.toFixed(2));
+    }
 
-    useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
 
-      function success(position) {
-        setLat(position.coords.latitude.toFixed(2));
-        setLong(position.coords.longitude.toFixed(2));
-      }
-  
-      navigator.geolocation.getCurrentPosition(success);
+    const getWeather = async () => {
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      )
+        .then((response) => response.json())
+        .then((data) => setWeather(data))
+        .catch((err) => console.log(err));
+    };
+    getWeather();
+  }, [latitude, longitude]);
 
-      const getWeather = async () => {
-        await fetch(`https://api.openweathermap.org/data/2.5/weather?&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`).then(
-          response =>
-            response.json()
-        ).then(data =>
-          setWeather(data)
-        ).catch(err => console.log(err));
-      }
-      getWeather();
-    }, [latitude, longitude]
-    );
-   
-    return (
-      <div className='App'>
-        <Weather weatherData={ weather } />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Weather weatherData={weather} />
+    </div>
+  );
+}
 export default App;
