a7b8494795840af2ee388f62e6709c2a8375966f
brutalnumberboy <scandalzx@protonmail.com>
2024-01-09 21:26:27 +0500

    added weather icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import { useEffect, useState } from "react";
 import AppCard from "./Weather.js";
 import { API_KEY } from "./OpenWeatherApi";
+export const WeatherIcon = {
+  "01d": "https://openweathermap.org/img/wn/01d@2x.png",
+  "02d": "https://openweathermap.org/img/wn/02d@2x.png",
+  "03d": "https://openweathermap.org/img/wn/03d@2x.png",
+  "04d": "https://openweathermap.org/img/wn/04d@2x.png",
+  "05d": "https://openweathermap.org/img/wn/05d@2x.png",
+  "06d": "https://openweathermap.org/img/wn/06d@2x.png",
+  "07d": "https://openweathermap.org/img/wn/07d@2x.png",
+  "08d": "https://openweathermap.org/img/wn/08d@2x.png",
+  "09d": "https://openweathermap.org/img/wn/09d@2x.png",
+  "10d": "https://openweathermap.org/img/wn/10d@2x.png",
+  "11d": "https://openweathermap.org/img/wn/11d@2x.png",
+  "01n": "https://openweathermap.org/img/wn/01n@2x.png",
+  "02n": "https://openweathermap.org/img/wn/02n@2x.png",
+  "03n": "https://openweathermap.org/img/wn/03n@2x.png",
+  "04n": "https://openweathermap.org/img/wn/04n@2x.png",
+  "05n": "https://openweathermap.org/img/wn/05n@2x.png",
+  "06n": "https://openweathermap.org/img/wn/06n@2x.png",
+  "07n": "https://openweathermap.org/img/wn/07n@2x.png",
+  "08n": "https://openweathermap.org/img/wn/08n@2x.png",
+  "09n": "https://openweathermap.org/img/wn/09n@2x.png",
+  "10n": "https://openweathermap.org/img/wn/10n@2x.png",
+  "11n": "https://openweathermap.org/img/wn/11n@2x.png"
+};
+
 function App() {
   const [latitude, setLat] = useState([]);
   const [longitude, setLong] = useState([]);
